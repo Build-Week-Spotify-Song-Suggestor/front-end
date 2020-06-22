eb09dfb8f26fee8c2d445a5c7854aa29062520f4
Mark Rivera <Mriver1991@gmail.com>
2020-06-22 12:42:24 -0400

    added bootstrap files, app is now rendering as intended

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Login from "./components/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // State Management
 import { connect } from "react-redux";
