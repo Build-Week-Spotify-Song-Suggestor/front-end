4030b9e54c972500adc33e33762f234357f21aed
Mark Rivera <Mriver1991@gmail.com>
2020-06-22 13:07:01 -0400

    added files to respective folders in components

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import Login from "./components/Login";
-import Navigation from "./components/Navigation";
+import Login from "./components/Login/Login";
+import Navigation from "./components/Navigation/Navigation";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // State Management
