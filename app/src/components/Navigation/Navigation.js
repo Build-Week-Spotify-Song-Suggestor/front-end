3ddc2fdcec5fe7e8123136dd90093d5b4b170c46
Mark Rivera <Mriver1991@gmail.com>
2020-06-22 14:38:57 -0400

    working on custom navigation

--- app/src/components/Navigation/Navigation.js
+++ app/src/components/Navigation/Navigation.js
@@ -1,33 +1,22 @@
 import React, { useState } from "react";
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-} from "reactstrap";
+import NavToggle from "./NavToggle";
+import "./Navigation.css";
+
+// React Router
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function Navigation() {
   const [collapsed, setCollapse] = useState(true);
   const toggleNavbar = () => setCollapse(!collapsed);
 
   return (
-    <Navbar color="info" light>
-      <NavbarBrand>Wave Suggester</NavbarBrand>
-      <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-      <Collapse isOpen={!collapsed} navbar>
-        <Nav navbar>
-          <NavItem>
-            <NavLink>About</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink>Contact</NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
-    </Navbar>
+    <Router>
+      <nav className="nav">
+        <div className="brand">Wave Suggester</div>
+
+        <NavToggle />
+      </nav>
+    </Router>
   );
 }
 
